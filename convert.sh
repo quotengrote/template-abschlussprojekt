7330b889df733bd3cfb04918b4499ca9d455bd45
Michael Grote <michael.grote@posteo.de>
2021-12-17 09:58:38 +0100

    convert.sh ergänzt

---
+++ convert.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+case "$1" in
+  --help)
+    echo "Konvertiere *.md zu einem PDF
+    ;;
+  "")
+    echo "Input-Datei fehlt"
+    ;;
+  *)
+    pandoc $1  --number-sections \
+      --pdf-engine=lualatex  \
+      -V papersize=a4paper  \
+      -V geometry:margin=3cm  \
+      -V lang=de-DE  \
+      -V breakurl  \
+      -V hyphens=URL  \
+      -V colorlinks \
+      --highlight-style tango \
+      -o output.pdf
+esac
