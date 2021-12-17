fa17e984e10abebec820f414502bb37d970a7aa5
Michael Grote <michael.grote@posteo.de>
2021-12-17 10:00:26 +0100

    convert als function

--- convert.sh
+++ convert.sh
@@ -1,20 +1,25 @@
 #!/bin/bash
+
+function convert {
+  pandoc $1  --number-sections \
+    --pdf-engine=lualatex  \
+    -V papersize=a4paper  \
+    -V geometry:margin=3cm  \
+    -V lang=de-DE  \
+    -V breakurl  \
+    -V hyphens=URL  \
+    -V colorlinks \
+    --highlight-style tango \
+    -o output.pdf
+}
+
 case "$1" in
   --help)
-    echo "Konvertiere *.md zu einem PDF
+    echo "Konvertiere *.md zu einem PDF"
     ;;
   "")
     echo "Input-Datei fehlt"
     ;;
   *)
-    pandoc $1  --number-sections \
-      --pdf-engine=lualatex  \
-      -V papersize=a4paper  \
-      -V geometry:margin=3cm  \
-      -V lang=de-DE  \
-      -V breakurl  \
-      -V hyphens=URL  \
-      -V colorlinks \
-      --highlight-style tango \
-      -o output.pdf
+    convert
 esac
