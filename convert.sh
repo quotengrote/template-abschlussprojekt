ba384a4d5986013c1c15fed1efafcfd664d88a3b
Michael Grote <michael.grote@posteo.de>
2022-01-11 17:24:50 +0100

    Header.tex -- + Tabellen alternierend gefärbt

--- convert.sh
+++ convert.sh
@@ -1,6 +1,56 @@
 #!/bin/bash
 
+
+
 function convert {
+  # create header.tex
+  cat > ./header.tex <<EOF
+% Tabellen linksbündig
+\usepackage{floatrow}
+\floatsetup[figure]{objectset=raggedright}
+\floatsetup[table]{objectset=raggedright}
+% Tabellenzeilen etwas großzügiger dimensionieren
+\renewcommand*{\arraystretch}{1.25}
+
+\usepackage[utf8]{inputenc}
+\usepackage[T1]{fontenc}
+\usepackage[ngerman]{babel}    % Trennungen nach neuer deutscher Rechtschreibung
+\usepackage{calc}              % einfache Arithmetik in latex
+\usepackage{longtable}         % Pandoc-Tabellen
+\usepackage[normalem]{ulem}    % für \sout (strike out)
+\usepackage[draft]{addlines}   % Seiten manuell größer machen mit \addline[1]
+\usepackage{textcomp}          % diverse Symbole, setzt Pandoc voraus
+
+\usepackage[document]{ragged2e}  % kein Blocksatz
+\parindent0pt           % verzichte auf Einrücken der ersten Zeile
+\parskip1.2ex           % vernünftiger Leerraum zw. Absätzen; früher 2
+\raggedbottom           %kein vertikaler Randausgleich nach unten
+\widowpenalty=10000     %Schusterjungen und Hurenkinder möglichst
+\clubpenalty=10000      %ausschließen
+\displaywidowpenalty = 10000
+\sloppy                 %Anzahl der Warnungen reduzieren (es bleiben genug!)
+\doublehyphendemerits=1000000  % zuviele Trennungen hintereinander vermeiden
+
+% alternierende Hintergrundfarben
+\usepackage[table]{xcolor}
+\definecolor{lightgray1}{gray}{0.88}
+\definecolor{lightgray2}{gray}{0.94}
+\let\oldlongtable\longtable
+\let\endoldlongtable\endlongtable
+\renewenvironment{longtable}%
+  {\rowcolors{2}{lightgray2}{lightgray1}\oldlongtable}%
+  {\endoldlongtable}
+
+% Farbe und Breite der Tabellenlinien
+\usepackage{colortbl}         
+\arrayrulecolor{black}%              
+\setlength\arrayrulewidth{1pt}% wirkt für /hline, nicht für /toprule etc.
+
+% Tabellenzeilen etwas großzügiger dimensionieren
+\renewcommand*{\arraystretch}{1.25}
+
+EOF
+
   pandoc "$1"  --number-sections \
     --pdf-engine=lualatex  \
     -V papersize=a4  \
@@ -11,8 +61,10 @@ function convert {
     -V breakurl  \
     -V hyphens=URL  \
     --highlight-style tango \
-    -H header.tex \
-    -o output.pdf 
+    -H ./header.tex \
+    -o output.pdf
+
+  rm header.tex
 }
 
 case "$1" in
@@ -23,5 +75,5 @@ case "$1" in
     echo "Input-Datei fehlt"
     ;;
   *)
-    convert "$1" 
+    convert "$1"
 esac
